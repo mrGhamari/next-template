412bd33428c79e96b7c2d5393c5e51f4192ca5b4
mmdrza <mmdrza77@gmail.com>
2026-02-15 13:43:39 +0330

    change title

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex min-h-[calc(100vh-8rem)] items-center justify-center px-4">
       <div className="bg-card flex w-full max-w-3xl flex-col items-center gap-4 rounded-2xl px-8 py-16 shadow-sm">
-        <h1 className="text-6xl">New Trend Panel</h1>
+        <h1 className="text-6xl">New next template</h1>
         <ModeToggle />
         <p className="text-2xl">{t('hello')}</p>
       </div>
